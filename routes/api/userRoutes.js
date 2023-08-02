5f106faf8452bcca17ad5c983b2554763a84ca73
RandyYanish <rtaylorfargo@gmail.com>
2023-08-02 15:16:23 -0700

    route update

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,6 +3,7 @@ const {
     getUsers,
     getSingleUser,
     createUser,
+    updateUser,
     deleteUser,
 } = require('../../controllers/userController.js');
 
@@ -13,6 +14,7 @@ router.route('/').get(getUsers).post(createUser);
 router
     .route('/:userId')
     .get(getSingleUser)
+    .put(updateUser)
     .delete(deleteUser);
 
 module.exports = router;
